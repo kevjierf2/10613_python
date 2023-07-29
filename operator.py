7aa1f560b0a7a31c40cbc2ab1b0783341290dc0a
User <twowonho23@gamil.com>
2023-07-29 11:33:47 +0900

    논리연산 학습

--- operator.py
+++ operator.py
@@ -1,3 +1,4 @@
+#사칙연산
 plus = 5+4
 minus = 10-7
 multipy = 20*3
@@ -16,3 +17,29 @@ n = 13 // 4
 
 print(r)
 print(n)
+
+#논리연산
+numA = 20
+numB = 4
+isepual =5== 4
+isNotEpual =5 != 4
+# 느낌표는 아니다,부정의 의미
+
+print(isepual)
+print(isNotEpual)
+
+isEpual = numA == 20
+
+print(isEpual)
+
+isBiggerThan= numA > 20
+isBiggerOrEpual Than = numA >= 20
+
+print(isBiggerThan)
+print(isBiggerOrEpual Than )
+
+issmallerThan =
+issmallerOrEpual Than =
+
+print(issmallerThan)
+print(issmallerOrEpual Than)
