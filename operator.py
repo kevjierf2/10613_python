e113171f4b7b6460c57f02bc02d1f0f7da9b7596
User <twowonho23@gamil.com>
2023-07-29 11:08:54 +0900

    print와 변수확인

---
+++ operator.py
@@ -0,0 +1,18 @@
+plus = 5+4
+minus = 10-7
+multipy = 20*3
+division = 9/3
+
+print(plus)
+print(minus)
+print(multipy)
+print(division)
+
+#error casse
+#divisionBYzero = 9/0
+
+r = 13 % 4
+n = 13 // 4
+
+print(r)
+print(n)
