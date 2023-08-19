7ca7c7b2231e5968c64ab29b7f58dd86353eed00
User <twowonho23@gmail.com>
2023-08-19 10:19:18 +0900

    사용자로부터 값을 입력받는 방법

---
+++ inputVariable .py
@@ -0,0 +1,7 @@
+first_number = input("숫자를 하나 입력하세요.") # input은 문자열을 입력 받아서 return 해줌
+temp_number = int(first_number) +10  # 이런 수식을 사용하고자 한다면 입력받은 변수를 int형으로 변환
+
+print(" 첫번째로는 숫자 ",first_number, "를 입력하셨군요!" )
+print(first_number, "에 10을 더한 값은", temp_number,"입니다!" )
+
+secon_number = int(input(" 다른 숫자도 입력해볼까요?")) #이런 형식으로 바로 치환도 가능
