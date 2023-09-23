46114cff55e3b984588247f70e2e9b9e8d67de43
User <twowonho23@gmail.com>
2023-09-23 10:30:59 +0900

    0826 문제풀이

--- whileStatement.py
+++ whileStatement.py
@@ -38,6 +38,22 @@ while True:
         print("정답입니다!")
         break
 
-        # 사용자가 answer 맞출때까지 반복
-        # 1. 사용자에게 기회주기
-        # 2. 틀렸을때 updown 출력해주기
+# 사용자가 answer 맞출때까지 반복
+# 1. 사용자에게 기회주기
+# 2. 틀렸을때 updown 출력해주기
+
+while chance > 0:
+    user_input =int(input("값을 입력하세요 >>"))
+
+    if user_input == answer:
+        print("정답입니다!")
+        break
+    else:
+        chance =chance -1
+        if user_input > answer:
+            print('down')
+        else:
+            print("up")
+
+
+
